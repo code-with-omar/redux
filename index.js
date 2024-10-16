af4d14ffb7fc326a7d9bd0b68074b550a1214d95
Omar Faruk <omarfaruk65142@gmail.com>
2024-10-16 12:31:29 +0600

    Counter app create

--- index.js
+++ index.js
@@ -1,70 +1,59 @@
-const { createStore } = require("redux")
+const { createStore } = require("redux");
 
-// defining constants
-const INCREMENT = "INCREMENT"
-const DECREMENT = "DECREMENT"
-const ADD_USER = "Hossain"
-//State
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
 const initialCounterState = {
-    count: 0,
-    name: "JavaScript"
-}
+  count: 0,
+};
 
-const initialUserState = {
-    users: [{
-        name: "Md. Omar Faruk"
-    }]
-}
-//action is a object-object may be two. One is type another is payload 
 const incrementCounter = () => {
-    return {
-        type: INCREMENT
-    }
-}
+  return {
+    type: INCREMENT,
+  };
+};
 const decrementCounter = () => {
-    return {
-        type: DECREMENT
-    }
-}
-
-const addUser = (user) => {
-    return {
-        type: ADD_USER,
-        payload: user
-    }
-}
-
-// create reducer for counter
+  return {
+    type: DECREMENT,
+  };
+};
+const resetCounter = () => {
+  return {
+    type: RESET,
+  };
+};
 
 const counterReducer = (state = initialCounterState, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state,
-                count: state.count + 1
-            }
-        case DECREMENT:
-            return {
-                ...state,
-                count: state.count - 1
-            }
-        default:
-            state
-            break;
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case RESET:
+      return {
+        ...state,
+        count: 0,
+      };
 
-    }
-}
+    default:
+      state;
+  }
+};
 
-const store = createStore(counterReducer)
+const store = createStore(counterReducer);
+
 store.subscribe(() => {
-    console.log(store.getState())
-  
-})
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(decrementCounter())
-// 1. State
-// 2. dispatch (action create korte hoy)
-// 3. reducer (action er type er upor kaj kore)
-// 4. store
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
+store.dispatch(resetCounter());
